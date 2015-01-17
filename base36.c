d5ca09d629f993b3c6cb427de1610eeb408a887f
ami-GS <1991.daiki@gmail.com>
2015-01-17 03:29:43 +0000

    implement num2base36

--- base36.c
+++ base36.c
@@ -24,8 +24,33 @@ unsigned int base36(char *s) {
   return ans;
 }
 
+void num2base36(char *dist, int num) {
+  int i = 0;
+  while (num / (int)pow(36.0, i) != 0) {
+    i++;
+  }
+
+  int tmp;
+  for (; i > 0; i--) {
+    tmp = num / (int)pow(36.0, i-1);
+    if (0 <= tmp && tmp <= 9) {
+      *dist = tmp + '0';
+    } else if (10 <= tmp && tmp <= 35) {
+      *dist = tmp + 'a' - 10;
+    }
+    dist++;
+    num %= (int)pow(36.0, i-1);
+  }
+  dist = '\0';
+
+}
+
 void main() {
   char *s = "1z";
   unsigned int ans = base36(s);
   printf("%d\n", ans);
+  char dist[BUFSIZ];
+  int num = 35*(int)pow(36,2) + 10*(int)pow(36,1) + 3;
+  num2base36(&dist[0], num);
+  printf("%s\n", dist);
 }
