7d7b80d2afee3a8b47f662c3c0e2902d1b61004c
ami-GS <1991.daiki@gmail.com>
2015-02-10 12:09:12 +0000

    added primeList function

--- Prime.c
+++ Prime.c
@@ -14,8 +14,36 @@ int isPrime(int num) {
   return 1;
 }
 
+void primeList(int *list) {
+  list[0] = -1;
+  list[1] = -1;
+  int i = 0, j;
+  while (list[i++] != '\0') {}
+  int len = i-1;
+
+  for (i = 2; i < len; i++) {
+    for (j = i*2; j < len; j += i) {
+      list[j] = -1;
+    }
+  }
+}
+
 void main() {
   int i;
   for (i = 0; i < 100; i++)
     printf("%d:%d\n", i, isPrime(i));
+
+  int list[100];
+  for (i = 0; i < 100; i++) {
+    list[i] = 1;
+  }
+
+  primeList(&list[0]);
+  for (i = 0; i < 100; i++) {
+    if (list[i] == 1) {
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
+
 }
